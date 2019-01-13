10508326368fb1600d1ac614859d7c2c9ff489cf
atkinsonholly <atkinsonholly@gmail.com>
2019-01-13 18:29:55 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,48 @@ const questions = [
 ]
 
 let question;
+
+//const p = document.createElement("div")
+
+const appendQuestion = function(question) {
+  document.querySelector(".question-container").innerHTML = question.questionText;
+}
+
+const askQuestionThen = function(time) {
+  question = questions[0];
+  appendQuestion(question);
+  return new Promise(function(resolve) {
+    setTimeout(function() {
+      resolve(question)
+    }, time )
+  });
+}
+
+const removeQuestion = function() {
+  return new Promise(function(question) {
+    document.querySelector(".question-container").innerHTML = "";
+    toggleTrueAndFalseButtons();
+  })
+}
+
+const askQuestionThenRemoveQuestion = function() {
+    return askQuestionThen(1000).then(removeQuestion)
+}
+
+const trueAndFalseButtons = function() {
+  return document.querySelector(".true-false-list").querySelectorAll(".btn")
+}
+
+const toggleTrueAndFalseButtons = function() {
+  trueAndFalseButtons().forEach(function(btn){
+    btn.classList.toggle("hide");
+  })
+}
+
+const displayQuestionOnClick = function() {
+  let btn = document.querySelector('a') //this is the main 'Ask' button
+ return btn.addEventListener('click', () => {
+   toggleTrueAndFalseButtons()
+   askQuestionThenRemoveQuestion(5000)
+ })
+}
